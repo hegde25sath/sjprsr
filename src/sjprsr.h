c9f4c6c5671718407f2ce808ad97ba02b2d8875a
Sathvik Hegde <hegde25@purdue.edu>
2024-12-28 15:13:14 +0530

    preliminary header file

---
+++ src/sjprsr.h
@@ -0,0 +1,19 @@
+/**
+ * Written by Sathvik Hegde, sathvik@purdue.edu
+ */
+
+#ifndef SJPRSR_H_SEEN
+#define SJPRSR_H_SEEN
+
+/* Constant definitions */
+
+/* Error codes */
+
+#define INVALID_JSON (0)
+#define VALID_JSON (1)
+
+/* Structure declarations */
+
+/* Function prototypes */
+
+#endif /* !SJPRSR_H_SEEN */
