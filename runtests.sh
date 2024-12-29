d238a017ac1d24f5a54776fbbe3ded0f19caac4e
Sathvik Hegde <hegde25@purdue.edu>
2024-12-29 21:11:25 +0530

    completed rudimentary bash test script that ruins test suite

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-CC=gcc
-maxtime=5
+#CC=gcc
+#maxtime=5
 
-# convention: 0: wrong json, 1: correct json
+INVALID_JSON=0
+VALID_JSON=1
+PASSED=0
+TOTAL=285
 
+echo "============= Tests ================="
 for testfile in $( ls ./tests/ ); do
-
+  FIRSTCHAR=${testfile:0:1}
+  ./sjprsr $testfile
+  RET=$?
+  if [[ $RET -eq $INVALID_JSON ]] && [[ $FIRSTCHAR == 'y' ]]; then
+    echo $testfile FAILED...
+    echo ----------------------------------
+  elif [[ $RET -eq $INVALID_JSON ]] && [[ $FIRSTCHAR == 'n' ]]; then
+    echo $testfile PASSED...
+    let PASSED=PASSED+1
+    echo ----------------------------------
+  elif [[ $RET -eq $VALID_JSON ]] && [[ $FIRSTCHAR == 'y' ]]; then
+    echo $testfile PASSED...
+    let PASSED=PASSED+1
+    echo ---------------------------------
+  elif [[ $RET -eq $VALID_JSON ]] && [[ $FIRSTCHAR == 'n' ]]; then
+    echo $testfile FAILED...
+    echo ---------------------------------
+  fi
 done
+echo "============ Summary ==================="
+echo "Total: " $PASSED " of " $TOTAL
